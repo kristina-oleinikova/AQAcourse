856649b05a6a597b438e6ce846f112786d8add08
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-06-24 22:53:36 +0400

    add updates

--- src/test/java/ParamTest.java
+++ src/test/java/ParamTest.java
@@ -1,11 +1,12 @@
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParamTest {
 
-    @Parameters({"", ""})
+    @Parameters({"Login-value", "Psw-value"})
     @Test
-    public void paramTest(String login, System psw){
+    public void paramTest(@Optional("Default login") String login, @Optional("Default psw") String psw){
         System.out.println("Login is " + login);
         System.out.println("Password is " + psw);
     }
