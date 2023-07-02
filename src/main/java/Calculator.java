e3ab83387728d3dfe7d81ea147c8331057b729b0
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-07-02 16:18:28 +0400

    fix comments

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,11 +5,11 @@ public class Calculator {
         return a+b;
     }
 
-    public int divInt(int a, int b){
+    public int div(int a, int b){
         return a/b;
     }
 
-    public double divDouble(double a, double b){
+    public double div(double a, double b){
         return a/b;
     }
 
--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,5 +1,7 @@
 import data.StaticProvider;
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class HomeWork extends BaseTest{
@@ -7,48 +9,70 @@ public class HomeWork extends BaseTest{
     @Test(groups = "smoke",priority = 1)
     public void simpleTest() {
         int expectedResult= 3;
-        Assert.assertEquals(calculator.divInt(6,2), expectedResult, "Values don't match");
+        Assert.assertEquals(calculator.div(6,2), expectedResult, "Values don't match");
     }
 
     @Test(description = "Divide integer", testName = "Division integer numbers", enabled = true)
     public void descriptionTest() {
         int expectedResult= 3;
-        Assert.assertEquals(this.calculator.divInt(6,2), expectedResult, "Values don't match");
+        Assert.assertEquals(calculator.div(6,2), expectedResult, "Values don't match");
     }
 
     @Test(testName = "Test with timeout", timeOut = 3000, dependsOnMethods = "simpleTest")
     public void TimeOutTest() throws InterruptedException {
         Thread.sleep(2500);
         int expectedResult= 3;
-        Assert.assertEquals(calculator.divInt(6,2), expectedResult, "Values don't match");
+        Assert.assertEquals(calculator.div(6,2), expectedResult, "Values don't match");
     }
 
     @Test(invocationCount = 2, invocationTimeOut = 1000)
     public void invocationCountTest() throws InterruptedException {
         Thread.sleep(500);
         int expectedResult= 3;
-        Assert.assertEquals(calculator.divInt(6,2), expectedResult, "Values don't match");
+        Assert.assertEquals(calculator.div(6,2), expectedResult, "Values don't match");
     }
 
     @Test(groups = "smoke", testName = "Divide Integer", dataProvider = "Data for division integer", dataProviderClass = StaticProvider.class)
     public void divideInteger(int a, int b, int expectedResult){
-        Assert.assertEquals(this.calculator.divInt(a,b), expectedResult, "Values don't match");
+        Assert.assertEquals(calculator.div(a,b), expectedResult, "Values don't match");
     }
 
     @Test(testName = "Divide Double", dataProvider = "Data for division double", dataProviderClass = StaticProvider.class)
     public void divideDouble(double a, double b, double expectedResult){
-        Assert.assertEquals(calculator.divDouble(a,b), expectedResult, "Values don't match");
+        Assert.assertEquals(calculator.div(a,b), expectedResult, "Values don't match");
     }
 
     @Test(groups = "regression", testName = "Division Integer by zero")
     public void divideByZero() throws ArithmeticException{
-        Assert.assertEquals(calculator.divInt(12, 0), ArithmeticException.class, "Values don't match");
+        Assert.assertEquals(calculator.div(12, 0), ArithmeticException.class, "Values don't match");
     }
 
-    @Test(groups = "regression", testName = "Divide by Zero Double")
-    public void divideByZeroDouble(){
-        Assert.assertEquals(calculator.divDouble(12.2, 0), Double.POSITIVE_INFINITY, "Values don't match");
+    @Test(groups = "regression", testName = "Double.POSITIVE_INFINITY test")
+    public void divideByZeroDoublePositive(){
+        Assert.assertEquals(calculator.div(12.2, 0), Double.POSITIVE_INFINITY, "Values don't match");
     }
 
+    @Test(groups = "regression", testName = "Double.NEGATIVE_INFINITY test")
+    public void divideByZeroDoubleNegative(){
+        Assert.assertEquals(calculator.div(-12.2, 0), Double.NEGATIVE_INFINITY, "Values don't match");
+    }
 
+    @Test(testName = "Double.NaN test")
+    public void divideByZeroNaN(){
+        double expectedResult = calculator.div(0.0,0);
+
+        Assert.assertEquals(Double.NaN, expectedResult);
+    }
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void exceptionTest(){
+        int expectedResult = calculator.div(2,0);
+    }
+
+    @Test
+    @Parameters({"a","b"})
+    public void parameterTest(@Optional("Default value") int c,@Optional("Default value") int d){
+        int expectedResult = 3;
+        Assert.assertEquals(calculator.div(c,d), expectedResult, "Values don't match");
+    }
 }
