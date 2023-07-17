e6b066e9f38cf4a4307b97c081f92a4e02d4846f
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-07-18 01:03:16 +0400

    add branch

---
+++ src/test/java/tests/NavigationTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import baseEntities.BaseTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.configuration.ReadProperties;
+
+public class NavigationTest extends BaseTest {
+    @Test
+    public void successLoginTest() {
+        driver.get(ReadProperties.getUrl());
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setEmail("atrostyanko@gmail.com");
+        loginPage.getPswInput().sendKeys("Qwertyu_1");
+        loginPage.getLogInButton().click();
+    }
+}
